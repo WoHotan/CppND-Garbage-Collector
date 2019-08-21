f599e3d9b4bea4b8f083fc07d5c6aaca73526e9a
richard <richard_wen@wistron.com>
2019-08-21 01:43:34 -0400

    Implement collect function

--- gc_pointer.h
+++ gc_pointer.h
@@ -155,7 +155,22 @@ bool Pointer<T, size>::collect(){
     // TODO: Implement collect function
     // LAB: New and Delete Project Lab
     // Note: collect() will be called in the destructor
-    return false;
+    bool memFreed = false;
+    typename std::list<PtrDetails<T> >::iterator p = refContainer.begin();
+    do{
+        for(; p != refContainer.end(); p++){
+            if(p->refcount > 0)continue;
+            if(p->memPtr){
+                if(p->isArray){
+                    delete[] p->memPtr;
+                }
+                else delete p->memPtr;
+                refContainer.erase(p--);
+            }
+        }
+    }while (p != refContainer.end());
+    
+    return memFreed;
 }
 
 // Overload assignment of pointer to Pointer.
